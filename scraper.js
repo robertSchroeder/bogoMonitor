82ea93b4449ec8f0acae18afaecc390680c7c6e0
Robert Schroeder <robertschroeder@Mac.lan>
2025-01-26 14:31:09 -0500

    set headless browser to 'true'

--- scraper.js
+++ scraper.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
 
 
     // Disabling infobars from the headless browser seems to make it a bit faster. 
-    const browser = await puppeteer.launch({headless:false,args: ['--disable-infobars']});
+    const browser = await puppeteer.launch({headless:true,args: ['--disable-infobars']});
     
     const page = await browser.newPage();
 
